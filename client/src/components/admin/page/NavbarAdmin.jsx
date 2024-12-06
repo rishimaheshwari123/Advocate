69f95dfa7fe1fe408768cd2301dc3a9c41db2db6
Rishi <rishimaheshwari040@gmail.com>
2024-12-06 20:16:57 +0530

    update

--- client/src/components/admin/page/NavbarAdmin.jsx
+++ client/src/components/admin/page/NavbarAdmin.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             sublinks: [
               { to: "/admin/addCompany", label: "Create Establishment" },
               { to: "/companies-login", label: "Company Login" },
-              { to: "#", label: "Licience" },
+              { to: "#", label: "Licence" },
               { to: "#", label: "Backup Data" },
               { to: "#", label: "Google Meet" },
               { to: "#", label: "Close Establishment" },
@@ -120,108 +120,106 @@ const Navbar = () => {
     dispatch(logout(navigate));
   };
   return (
-    <div className="fixed top-0 left-0 w-full z-50  flex h-[140px]">
-      <nav className="flex items-center justify-between py-3 w-[85%] ">
-        <div className="flex-1">
-          <div className="min-w-full flex text-2xl mt-5 justify-center py-3 items-center bg-[#b1d7e0] shadow-inner shadow-gray-500">
-            <p
-              className="p-2 font-bold text-2xl"
-              style={{
-                textShadow: "2px 2px 4px yellow",
-              }}
-            >
-              {company ? company?.companyName : "S.D. Taxation Associate"}
-            </p>
-          </div>
+    <div className="fixed top-0 left-0 w-full z-50   h-[140px]">
+      <div>
+        <nav className="flex items-center justify-between  w-screen  ">
+          <div className="flex-1">
+            <div className="min-w-full flex justify-center items-center text-2xl  py-14 bg-[#b1d7e0] shadow-inner shadow-gray-500 relative">
+              <p
+                className="p-2 font-bold text-5xl text-center absolute left-1/2 transform -translate-x-1/2"
+                style={{
+                  textShadow: "2px 2px 4px yellow",
+                }}
+              >
+                {company ? company?.companyName : "S.D. Taxation Associate"}
+              </p>
 
-          <div className="h-[40px] my-[6px]">
-            <div className="p  flex justify-between items-center h-full font-bold text-[17px]">
-              <div className="bg-[#fcd5b5] shadow-lg shadow-gray-500 px-3 py-1">
-                Client Grievance Alert{" "}
-                <span className="text-red-600">{">>>"}</span>
-              </div>
-              <div className="bg-[#fcd5b5] px-3 py-1 shadow-lg shadow-gray-500 border min-w-[62%] m text-center ">
-                Alerts--!
-              </div>
-              <div className="flex gap-4">
-                <span className="border p-1 shadow-lg  shadow-gray-500 bg-[#fcd5b5] px-3 ">
-                  {currentDate}
-                </span>
-                <span className="border p-1 shadow-lg  shadow-gray-500 bg-[#fcd5b5] px-3 ">
-                  {currentTime}
-                </span>
+              <div className="absolute right-4 flex flex-col gap-1 font-semibold bg-white p-1">
+                <p className="bg-red-900 text-white border border-white py-1 px-2 text-center">
+                  {company ? company.companyName : "S.D. Taxation Ass."}
+                </p>
+                <button
+                  onClick={handleLogout}
+                  className="bg-red-900 text-white border border-white py-1 flex items-center justify-center gap-2"
+                >
+                  <MdLogout /> Logout
+                </button>
               </div>
             </div>
-          </div>
 
-          <ul className="grid grid-cols-9  space-x-2 relative mt- justify-between">
-            {navItems.map((item, index) => (
-              <li
-                key={index}
-                className="relative group"
-                onMouseEnter={() => setHoveredItem(index)}
-                onMouseLeave={() => setHoveredItem(null)}
-              >
-                <div className="bg-white p-[2px] shadow-lg shadow-gray-600">
-                  <NavLink
-                    to={"#"}
-                    className={({ isActive }) =>
-                      `flex  justify-center font-semibold items-center gap-2 px-3 py-2 text-lg bg-[#b1d7e0] shadow-inner shadow-gray-500 border ${
-                        isActive
-                          ? "border-b-2 border-white"
-                          : "hover:border-b-2 hover:border-gray-400"
-                      }`
-                    }
-                  >
-                    {/* <span>{item.icon}</span> */}
-                    <span className="font-bold">{item.label}</span>
-                  </NavLink>
+            <div className="h-[40px] my-[6px]">
+              <div className="px-1  flex justify-between items-center h-full font-bold text-[17px]">
+                <div className="bg-[#fcd5b5] rounded-sm shadow-lg shadow-gray-500 px-3 py-1">
+                  Client Grievance Alert{" "}
+                  <span className="text-red-600">{">>>"}</span>
                 </div>
-                {item.sublinks.length > 0 && hoveredItem === index && (
-                  <ul className="absolute top-full left-0 bg-white b shadow-md border rounded w-48 z-50">
-                    {item.sublinks.map((sublink, subIndex) => (
-                      <li
-                        key={subIndex}
-                        className=" bg-gray-200 hover:bg-gray-100  mx-1 "
-                      >
-                        <NavLink
-                          to={sublink.to}
-                          className="block px-4 font-bold py-2 my-2 shadow-inner shadow-black text-sm text-black "
+                <div className="bg-[#fcd5b5] rounded-sm px-3 py-1 shadow-lg shadow-gray-500 border min-w-[62%] m text-center ">
+                  Alerts--!
+                </div>
+                <div className="">
+                  <span className="border p-1 shadow-lg rounded-sm shadow-gray-500 bg-[#fcd5b5] px-3 ">
+                    {currentDate} <span className="mr-10"></span>
+                    {currentTime}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <ul className="grid grid-cols-9  space-x-2 relative mt- justify-between">
+              {navItems.map((item, index) => (
+                <li
+                  key={index}
+                  className="relative group"
+                  onMouseEnter={() => setHoveredItem(index)}
+                  onMouseLeave={() => setHoveredItem(null)}
+                >
+                  <div className="bg-white rounded-[4px] p-[2px] shadow-lg shadow-gray-600">
+                    <NavLink
+                      to={"#"}
+                      className={({ isActive }) =>
+                        `flex  justify-center  rounded-[4px] font-semibold items-center gap-2 px-3 py-2 text-lg bg-[#b1d7e0] shadow-inner shadow-gray-500 border ${
+                          isActive
+                            ? "border-b-2 border-white"
+                            : "hover:border-b-2 hover:border-gray-400"
+                        }`
+                      }
+                    >
+                      {/* <span>{item.icon}</span> */}
+                      <span className="font-bold ">{item.label}</span>
+                    </NavLink>
+                  </div>
+                  {item.sublinks.length > 0 && hoveredItem === index && (
+                    <ul className="absolute top-full left-0 bg-white rounded-sm shadow-md border  w-48 z-50">
+                      {item.sublinks.map((sublink, subIndex) => (
+                        <li
+                          key={subIndex}
+                          className=" bg-gray-200 hover:bg-gray-100  mx-1 "
                         >
-                          {sublink.label}
-                        </NavLink>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="fixed bottom-[4px]    w-[85%]">
-          <button className="bg-[#b1d7e0]   shadow-inner  shadow-gray-500 text-black font-semibold px-4 py-4   w-full ">
-            Wish
-          </button>
-        </div>
-      </nav>
-      <div className="w-[15%] min-h-screen  pt-1 px-2 text-center">
-        <div className=" flex flex-col gap-3 font-semibold">
-          <p className=" bg-red-900 text-white border border-white p-2 ">
-            {company ? company.companyName : "S.D. Taxation Ass."}
-          </p>
-          <button
-            onClick={handleLogout}
-            className=" bg-red-900 text-white border border-white p-2  flex items-center justify-center gap-2"
-          >
-            <MdLogout /> Logout
-          </button>
-        </div>
+                          <NavLink
+                            to={sublink.to}
+                            className="block px-4 font-bold py-2 my-2 shadow-inner  rounded-sm shadow-black text-sm text-black "
+                          >
+                            {sublink.label}
+                          </NavLink>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </nav>
+      </div>
 
-        <div className="bg-[#fcd5b5] min-w-full min-h-full mt-3 border rounded shadow-2xl">
-          <p className=" text-red-700 my-2 font-bold underline"> Wishes</p>
-        </div>
-
-        <div></div>
+      <div className="flex justify-end">
+        <div className="bg-[#fcd5b5] w-[15%] h-[60vh] mt-3 "></div>
+      </div>
+      <div className="fixed bottom-[4px] flex gap-2 w-screen   ">
+        <button className="bg-[#b1d7e0] text-start  shadow-inner  shadow-gray-500 text-black font-semibold px-4 py-4   w-full ">
+          Wishes Aleart {">>>"}
+        </button>
+        <button className="bg-[#b1d7e0]   shadow-inner  shadow-gray-500 text-black font-semibold px-4 py-4   w-full "></button>
       </div>
     </div>
   );
